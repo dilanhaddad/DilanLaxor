463fec6d76c6d81723b60f60c43bd3a3a9a322c2
Dilan <dilanhaddad@outlook.com>
2019-02-25 11:02:17 +0100

    Gissa tal
    jag har fixat en switch case så att man kan välja olika nivåer med olika svårigheter

--- v8/gissaTal.java
+++ v8/gissaTal.java
@@ -2,17 +2,46 @@ import java.util.Scanner;
 
 public class gissaTal
 {
-  public static void main(String args[])
-  {
-   Scanner input = new Scanner(System.in);
+  public static void main(String args[]){
+	  int max=0;
+	  int min=0;
+	  int guess = 0, count = 0, countMax = 0;
+	  System.out.println("Välj en nivå \nNivåer: \nNivå 1: 10 gissningar, maxtal 50 \nNivå 2: 10 gissnigar, maxtal 100 \nNivå 3: 16 gissningar, maxtal 200" );
+	  
+	   Scanner input = new Scanner(System.in);
+	
+	   int levels = input.nextInt();
+	 
+	  while (levels != 1 && levels != 2 && levels != 3) {
+		  System.out.println("Du måste välja en av nivåer");
+		  
+		  levels = input.nextInt();
+		
+	  }
+	  switch(levels) {
+	  case 1:
+		  countMax=10;
+		  min=0;
+		   max = 50;
+		   break;
+	  case 2:
+		  countMax=10;
+		  min=0;
+		   max = 100;
+		   break;
+	  case 3:
+		  countMax=16;
+		  min=0;
+		   max = 200;
+		   break;
+	  default:
+		  System.out.println("Detta är inte en nivå");
+	  }
+	  System.out.println("Du valde nivå " + levels + ", du har " + count + " gissningar, mellan talen " + min + " och " + max);
+	  
    
-   int min = 0;
-   int max = 100;
-   int a = 1 + (int) (Math.random() * 99),
-       guess, 
-       count = 0;
-
-   System.out.println("Gissa en siffra mellan 0 till 100?, du har 7 försök");
+   
+   int a = 1 + (int) (Math.random() * 99);
 
    while((guess = input.nextInt()) != a){
 	   
@@ -32,7 +61,7 @@ public class gissaTal
        System.out.println("högre!");
      }
      count++;
-     if (count >= 7) 
+     if (count >= countMax) 
      {
     	 System.out.println("Bra försökt men du gissade tyvärr inte rätt, talet var " + a);
     	 break;
