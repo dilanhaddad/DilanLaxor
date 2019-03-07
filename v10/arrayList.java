392cc97cc6bf0e55cb8ab4990541e0a3b24e3caa
Dilan <dilanhaddad@outlook.com>
2019-03-07 14:04:47 +0100

    ggissa tal

---
+++ v10/arrayList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class arrayList {
+
+	public static void main(String[] args) {
+		
+	int [] arr1 = {1,2,3,4,5};
+	String[] str1 = {"dilan","haddad"};
+	
+	for (int i = 0; i < str1.length; i++) {
+		System.out.println(arr1[i]);
+		
+		
+		
+	}
+	ArrayList<String> nameList = new ArrayList<>();
+	
+	nameList.add("Jonny");
+	nameList.add("Musa");
+	
+	for (int i = 0; i < nameList.size(); i++) {
+		System.out.print(nameList.get(i)+ " ");
+	}
+	} 
+
+}
--- v8/gissaTal.java
+++ v8/gissaTal.java
@@ -2,75 +2,70 @@ import java.util.Scanner;
 
 public class gissaTal
 {
-  public static void main(String args[]){
-	  int max=0;
-	  int min=0;
-	  int guess = 0, count = 0, countMax = 0;
-	  System.out.println("Välj en nivå \nNivåer: \nNivå 1: 10 gissningar, maxtal 50 \nNivå 2: 10 gissnigar, maxtal 100 \nNivå 3: 16 gissningar, maxtal 200" );
-	  
-	   Scanner input = new Scanner(System.in);
-	
-	   int levels = input.nextInt();
-	 
-	  while (levels != 1 && levels != 2 && levels != 3) {
-		  System.out.println("Du måste välja en av nivåer");
-		  
-		  levels = input.nextInt();
-		
-	  }
-	  switch(levels) {
-	  case 1:
-		  countMax=10;
-		  min=0;
-		   max = 50;
-		   break;
-	  case 2:
-		  countMax=10;
-		  min=0;
-		   max = 100;
-		   break;
-	  case 3:
-		  countMax=16;
-		  min=0;
-		   max = 200;
-		   break;
-	  default:
-		  System.out.println("Detta är inte en nivå");
-	  }
-	  System.out.println("Du valde nivå " + levels + ", du har " + count + " gissningar, mellan talen " + min + " och " + max);
-	  
-   
-   
-   int a = 1 + (int) (Math.random() * 99);
+	public static void main(String args[]) {
+		int max = 0;
+		int min = 0;
+		int guess = 0, count = 0, countMax = 0;
+		System.out.println(
+				"Välj en nivå \n \nNivå 1: 10 gissningar, maxtal 50 \nNivå 2: 10 gissnigar, maxtal 100 \nNivå 3: 16 gissningar, maxtal 200");
+		// Här har jag skrivit så att man kan veta vad man har för vinå, hur många gissningar och mellan vilka tal
+		Scanner input = new Scanner(System.in);
 
-   while((guess = input.nextInt()) != a){
-	   
-     if (guess > max)
-     {
-       System.out.println("Talet är under 100");
-     }
-     else if (guess < min)
-     {
-       System.out.println("Talet är högre än 0");
-     }
-     else if (guess > a){
-    	 System.out.println("lägre!");
-     }
-     else
-     {
-       System.out.println("högre!");
-     }
-     count++;
-     if (count >= countMax) 
-     {
-    	 System.out.println("Bra försökt men du gissade tyvärr inte rätt, talet var " + a);
-    	 break;
-     }
-   }
+		int levels = input.nextInt();
 
-   if(guess==a) {
-	   System.out.println("Rätt, bra jobbat!. Du gissade med "+ count +" försök!");
-   }
-  }
+		while (levels != 1 && levels != 2 && levels != 3) {
+			System.out.println("Du måste välja en av nivåer");
+
+			levels = input.nextInt();
+			// Detta är en loop för att man inte ska kunna välja en nivå högre än 3
+		}
+		switch (levels) {
+		case 1:
+			countMax = 10;
+			min = 0;
+			max = 50;
+			break;
+		case 2:
+			countMax = 10;
+			min = 0;
+			max = 100;
+			break;
+		case 3:
+			countMax = 16;
+			min = 0;
+			max = 200;
+			break;
+		default:
+			System.out.println("Detta är inte en nivå");
+			// detta är en switch case för nivåerna
+		}
+		System.out.println(
+				"Du valde nivå " + levels + ", du har " + countMax + " gissningar, mellan talen " + min + " och " + max);
+		// detta säger hur många gissningar och mellan vilka tal för din valda nivå
+
+		int a = 1 + (int) (Math.random() * 99);
+
+		while ((guess = input.nextInt()) != a) {
+
+			if (guess > max) {
+				System.out.println("Talet är under 100");
+			} else if (guess < min) {
+				System.out.println("Talet är högre än 0");
+			} else if (guess > a) {
+				System.out.println("lägre!");
+			} else {
+				System.out.println("högre!");
+			}
+			count++;
+			if (count >= countMax) {
+				System.out.println("Bra försökt men du gissade tyvärr inte rätt, talet var " + a);
+				break;
+			}
+		}
+
+		if (guess == a) {
+			System.out.println("Rätt, bra jobbat!. Du gissade med " + count + " försök!");
+		}
+	}
 
 }
