5e98fb027e48e84449a21418d13324b1765d2bfb
Dilan <dilanhaddad@outlook.com>
2019-03-21 14:11:27 +0100

    Update SecureDoors.java
    nu är allt färdigt

--- v10/SecureDoors.java
+++ v10/SecureDoors.java
@@ -5,25 +5,35 @@ public class SecureDoors {
 	public static void main (String[]args) {
 		
 	Scanner input = new Scanner (System.in);
-	System.out.println("Hur många händelser ska ske?");
 	int howMany = input.nextInt();
-	ArrayList<String> building = new ArrayList<>(); //denna listan håller koll på vilka som har gått ingenom dörren
+	ArrayList<String> building = new ArrayList<>(); //denna listan haller koll pa vilka som har gatt ingenom dorren
 	
-	for (int i = 0; i < howMany; i++) { //loop för att bestämma om personen gick in eller ut 
+	for (int i = 0; i < howMany; i++) { //loop for att bestamma om nagon gick ut eller in
         String track = input.next();
         String name = input.next();
-        if (track.equals("entry")) {
+        if (track.equals("entry")) { // for att om en person gick in
             System.out.print(name + " entered");
-            if (building.contains(name))
+            if (building.contains(name))// for om nagot konstigt hander sa kommer det skrivas ut "anomaly"
                 System.out.println(" (ANOMALY)");
-      
+            else {
+                building.add(name);
+                System.out.println();
+            }
+        }
+            else {// for om nagon gick ut
+                System.out.print(name + " exited");
+                if (!building.contains(name))//om nagot konstigt sa skriv "anomaly" ut
+                    System.out.println(" (ANOMALY)");
+                else {
+                    building.remove(name);
+                    System.out.println();
                 }	
             }
         
 	}
-	
+	input.close();	// sa att man inte kan skriva in mer i konsolen
 	}
-
+}
 
 
 
