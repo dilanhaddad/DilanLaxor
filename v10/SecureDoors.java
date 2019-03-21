98f39ef341afac53d671db2f9e1e3a9c7de5e455
Dilan <dilanhaddad@outlook.com>
2019-03-21 13:43:36 +0100

    secure door
    jag har fixat så att man kan entera en person och att det kan bli anomaly

--- v10/SecureDoors.java
+++ v10/SecureDoors.java
@@ -5,14 +5,25 @@ public class SecureDoors {
 	public static void main (String[]args) {
 		
 	Scanner input = new Scanner (System.in);
-	int antal = input.nextInt();
-	ArrayList<String> pers = new ArrayList<>();
+	System.out.println("Hur många händelser ska ske?");
+	int howMany = input.nextInt();
+	ArrayList<String> building = new ArrayList<>(); //denna listan håller koll på vilka som har gått ingenom dörren
 	
-	for (int i = 0; i < args.length; i++) {
-		
+	for (int i = 0; i < howMany; i++) { //loop för att bestämma om personen gick in eller ut 
+        String track = input.next();
+        String name = input.next();
+        if (track.equals("entry")) {
+            System.out.print(name + " entered");
+            if (building.contains(name))
+                System.out.println(" (ANOMALY)");
+      
+                }	
+            }
+        
 	}
 	
-	
 	}
-}
+
+
+
 
