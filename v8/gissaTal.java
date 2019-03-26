1ebf5fdf7407a0af60b6f733be2ab572fa7d093c
Dilan <dilanhaddad@outlook.com>
2019-03-26 10:42:12 +0100

    Update gissaTal.java

--- v8/gissaTal.java
+++ v8/gissaTal.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class gissaTal
 {
 	public static void main(String args[]) {
+		boolean restart = true;
+		while (restart) { // en loop för om man vill spela om
+		
 		int max = 0;
 		int min = 0;
-		int guess = 0, count = 0, countMax = 0;
+		int guess = 0, count = 1, countMax = 0;
+		String start;
 		System.out.println(
 				"Välj en nivå \n \nNivå 1: 10 gissningar, maxtal 50 \nNivå 2: 10 gissnigar, maxtal 100 \nNivå 3: 16 gissningar, maxtal 200");
 		// Här har jag skrivit så att man kan veta vad man har för vinå, hur många gissningar och mellan vilka tal
@@ -43,12 +47,12 @@ public class gissaTal
 				"Du valde nivå " + levels + ", du har " + countMax + " gissningar, mellan talen " + min + " och " + max);
 		// detta säger hur många gissningar och mellan vilka tal för din valda nivå
 
-		int a = 1 + (int) (Math.random() * 99);
+		int a = 1 + (int) (Math.random() * max);
 
 		while ((guess = input.nextInt()) != a) {
 
 			if (guess > max) {
-				System.out.println("Talet är under 100");
+				System.out.println("Talet är under ");
 			} else if (guess < min) {
 				System.out.println("Talet är högre än 0");
 			} else if (guess > a) {
@@ -67,7 +71,16 @@ public class gissaTal
 		if (guess == a) {
 			System.out.println("Rätt, bra jobbat!. Du gissade med " + count + " försök!");
 			// detta säget om du har rätt och hur många gånger du gissade
+		} 
+		System.out.println("\n");//om du vill köra om så ska du svara nej
+		System.out.println("Vill du spela om? \nSvara 'nej' om du inte vill starta om");
+		input.nextLine(); //buffer
+		start = input.nextLine();
+		if (start.equals("nej")) {
+			restart = false;
+			System.out.println("Tack för att du spelade!");
 		}
 	}
 
+}
 }
